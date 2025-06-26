19acb2b88de94f65daf7e0540d00224e13ddef9b
Pietro Bertera <pietro@bertera.it>
2025-06-26 17:05:14 +0200

    .

---
+++ probe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SLEEP=${SLEEP-1}
+URL=${URL-www.google.com}
+PORT=${PORT-80}
+SCHEMA=${SCHEMA-http}
+CONNECT=${URL}:${PORT}:${URL}:${PORT}
+LOGDIR="${LOGDIR-.}"
+LOGFILE="${LOGDIR}/${HOSTNAME}_probe_--connect-to_${CONNECT}_${SCHEMA}:${URL}.log"
+
+echo '"Date","HTTP Code","Remote IP","Time Total(s)","Size Total(bytes)","DNS Lookup(s)","TCP Connect time(s)","TLS Connect time(s)","Redirect(s)","Pre Transfer(s)","Start Transfer(s)"' | tee -a "$LOGFILE"
+
+probe(){
+  while true; do
+    curl -w "\"$(date -u)\",\"%{http_code}\",\"%{remote_ip}\",\"%{time_total}\",\"%{size_download}\",\"%{time_namelookup}\",\"%{time_connect}\",\"%{time_appconnect}\",\"%{time_redirect}\",\"%{time_pretransfer}\",\"%{time_starttransfer}\"\n" -o /dev/null -s $@
+  sleep $SLEEP
+  done
+}
+
+probe --connect-to "$CONNECT" "${SCHEMA}://${URL}" | tee -a "$LOGFILE"
