9496a6f582766ac6eacb32b2b57bc62beff29073
Pietro Bertera <pietro@bertera.it>
2025-07-07 12:18:18 +0200

    .

--- probe.sh
+++ probe.sh
@@ -7,7 +7,7 @@ SCHEMA=${SCHEMA-http}
 CONNECT=${URL}:${PORT}:${URL}:${PORT}
 LOGDIR="${LOGDIR-.}"
 NODENAME=${NODENAME-}
-LOGFILE="${LOGDIR}/${NODENAME}_${HOSTNAME}_probe_--connect-to_${CONNECT}_${SCHEMA}:${URL}.log"
+LOGFILE="${LOGDIR}/probe.log"
 
 echo '"Date","HTTP Code","Redirect URL","Remote IP","Time Total(s)","Size Total(bytes)","DNS Lookup(s)","TCP Connect time(s)","TLS Connect time(s)","Redirect(s)","Pre Transfer(s)","Start Transfer(s)"' | tee -a "$LOGFILE"
 
