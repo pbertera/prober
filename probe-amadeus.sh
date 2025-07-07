fe7609128c1b02130d11fc783a0cb46583e8b291
Pietro Bertera <pietro@bertera.it>
2025-07-07 11:43:33 +0200

    .

--- probe-amadeus.sh
+++ probe-amadeus.sh
@@ -8,13 +8,15 @@ CONNECT=${URL}:${PORT}:${URL}:${PORT}
 LOGDIR="${LOGDIR-.}"
 NODENAME=${NODENAME-}
 LOGFILE="${LOGDIR}/${NODENAME}_${HOSTNAME}_probe_--connect-to_${CONNECT}_${SCHEMA}:${URL}.log"
-MESSAGE="PZAVI               195.27.163.89  2002WSLPIALP            TRNIG3116           46duStqH                                YNALP30       0060                                                               W021809032644029371040323000108999999                                                                              NX8R5TJFK END-OF-BUFFER"
+MESSAGEB64="1+nB5clA8fn1S/L3S/H280v4+UDy8PDy5uLT18nB09dA49nVycfz8fH2QPT2hKTio5jIQOjVwdPX
+8/BA8PD28EDm8PLx+PD58PPy9vT08PL58/fx8PTw8/Lz8PDw8fD4+fn5+fn5QNXn+Nn149HG0kDF
+1cRg1sZgwuTGxsXZ"
 
 echo '"Date","HTTP Code","Redirect URL","Remote IP","Time Total(s)","Size Total(bytes)","DNS Lookup(s)","TCP Connect time(s)","TLS Connect time(s)","Redirect(s)","Pre Transfer(s)","Start Transfer(s)"' | tee -a "$LOGFILE"
 
 probe(){
   while true; do
-    echo -ne ${MESSAGE} | iconv -f UTF-8 -t IBM280 | curl -w "\"$(date -u)\",\"%{http_code}\",\"%{redirect_url}\",\"%{remote_ip}\",\"%{time_total}\",\"%{size_download}\",\"%{time_namelookup}\",\"%{time_connect}\",\"%{time_appconnect}\",\"%{time_redirect}\",\"%{time_pretransfer}\",\"%{time_starttransfer}\"\n" -o /dev/null -s $@
+    echo -ne ${MESSAGE} | base64 -d | curl -w "\"$(date -u)\",\"%{http_code}\",\"%{redirect_url}\",\"%{remote_ip}\",\"%{time_total}\",\"%{size_download}\",\"%{time_namelookup}\",\"%{time_connect}\",\"%{time_appconnect}\",\"%{time_redirect}\",\"%{time_pretransfer}\",\"%{time_starttransfer}\"\n" -o /dev/null -s $@
   sleep $SLEEP
   done
 }
