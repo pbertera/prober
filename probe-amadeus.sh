2815e7d3419a402af1a349729c1473bd6bf0ff31
Pietro Bertera <pietro@bertera.it>
2025-07-07 15:52:26 +0200

    .

--- probe-amadeus.sh
+++ probe-amadeus.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+set -x
+
 SLEEP=${SLEEP-1}
-URL=${URL-www.google.com}
+HOST=${HOST-www.google.com}
 PORT=${PORT-80}
-SCHEMA=${SCHEMA-http}
-CONNECT=${URL}:${PORT}:${URL}:${PORT}
 LOGDIR="${LOGDIR-.}"
 NODENAME=${NODENAME-}
-LOGFILE="${LOGDIR}/${NODENAME}_${HOSTNAME}_probe_--connect-to_${CONNECT}_${SCHEMA}:${URL}.log"
+TIMEOUT=1
+LOGFILE="${LOGDIR}/probe.log"
 MESSAGEB64="1+nB5clA8fn1S/L3S/H280v4+UDy8PDy5uLT18nB09dA49nVycfz8fH2QPT2hKTio5jIQOjVwdPX
 8/BA8PD28EDm8PLx+PD58PPy9vT08PL58/fx8PTw8/Lz8PDw8fD4+fn5+fn5QNXn+Nn149HG0kDF
 1cRg1sZgwuTGxsXZ"
 
-echo '"Date","HTTP Code","Redirect URL","Remote IP","Time Total(s)","Size Total(bytes)","DNS Lookup(s)","TCP Connect time(s)","TLS Connect time(s)","Redirect(s)","Pre Transfer(s)","Start Transfer(s)"' | tee -a "$LOGFILE"
-
 probe(){
   while true; do
-    echo -ne "${MESSAGEB64}" | base64 -d | curl -s $@
-    #echo -ne "${MESSAGEB64}" | base64 -d | curl -w "\"$(date -u)\",\"%{http_code}\",\"%{redirect_url}\",\"%{remote_ip}\",\"%{time_total}\",\"%{size_download}\",\"%{time_namelookup}\",\"%{time_connect}\",\"%{time_appconnect}\",\"%{time_redirect}\",\"%{time_pretransfer}\",\"%{time_starttransfer}\"\n" -o /dev/null -s $@
+    timeout 5 echo -n "${MESSAGEB64}" | base64 -d | nc -w ${TIMEOUT} $@
+    if [ "$?" == "0" ]; then
+      echo "$(date -u) OK"
+    else
+      echo "$(date -u) KO"
+    fi
   sleep $SLEEP
   done
 }
 
-probe --connect-to "$CONNECT" "${SCHEMA}://${URL}" | tee -a "$LOGFILE"
+probe ${HOST} ${PORT} | tee -a "$LOGFILE"
